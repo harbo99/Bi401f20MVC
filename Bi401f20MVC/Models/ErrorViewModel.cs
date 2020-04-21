bca3a5b9606bb038f6a06ee22ae85dcec93dbe31
Nicolai Harbo Christensen <nhch18@student.aau.dk>
2020-04-21 09:31:39 +0200

    Add project files.

---
+++ Bi401f20MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bi401f20MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
